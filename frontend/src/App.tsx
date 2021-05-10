5d7c3d8bc8e7b11f46664128e4aa6c399e360810
Gabriel Veloso <gabriel.azuirson@ufpe.br>
2021-05-10 12:48:40 -0300

    Security Config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,11 +13,11 @@ function App() {
         <h1 className="text-primary py-4">Sales Dashboard</h1>
         <div className="row px-3">
           <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Success rate per saler (%)</h5>
+            <h5 className="text-center text-secondary">Success rate per seller (%)</h5>
             <BarChart />
           </div>
           <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Success rate per saler (%)</h5>
+            <h5 className="text-center text-secondary">Success rate per seller (%)</h5>
             <DonutChart />
           </div>
         </div>
