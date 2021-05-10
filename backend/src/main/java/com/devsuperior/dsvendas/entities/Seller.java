d7f928f1554c04360cf509128642b670dca54d88
Gabriel Veloso <gabriel.azuirson@ufpe.br>
2021-05-10 18:15:47 -0300

    layers

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -34,6 +34,32 @@ public class Seller {
 		this.id = id;
 		this.name = name;
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Sale> getSales() {
+		return sales;
+	}
+
+	public void setSales(List<Sale> sales) {
+		this.sales = sales;
+	}
+	
+	
 	
 	
 
